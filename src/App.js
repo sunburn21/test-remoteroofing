2dc7a85c1f6ec4bddd664c13e5d3f0011a96f265
sunburn21 <sunnythesingh21@gmail.com>
2020-06-01 21:44:19 +0530

    added basic header & footer

--- src/App.js
+++ src/App.js
@@ -9,17 +9,21 @@ import Home from './components/home';
 import Series from './components/series';
 import Movies from './components/movies';
 import Error from './components/error';
+import Header from './components/header';
+import Footer from './components/footer';
 
 class App extends Component{
   render(){
     return (
       <Router>
+        <Header/>
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/series' exact component={Series} />
           <Route path='/movies' exact component={Movies} />
           <Route component={Error}/>
         </Switch>
+        <Footer/>
       </Router>
     )
   }
